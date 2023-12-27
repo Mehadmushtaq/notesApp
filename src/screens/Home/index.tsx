02e278ef922c4805575446ad80c1bc5e14b92d1e
Mehad Mushtaq <mehadmushtaq@gmail.com>
2023-12-27 09:32:12 +0500

    fix: pdf displayed

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,127 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 function Home(): React.JSX.Element {
   const navigation = useNavigation<any>();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {
+      label: 'Apple',
+      value: 'apple',
+      icon: () => (
+        <Image
+          source={require('../../../src/assets/icons/icon-apple.png')}
+          style={{height: 20, width: 20}}
+        />
+      ),
+    },
+    {
+      label: 'Facebook',
+      value: 'facebook',
+      icon: () => (
+        <Image
+          source={require('../../../src/assets/icons/icon-facebook.png')}
+          style={{height: 20, width: 20}}
+        />
+      ),
+    },
+    {
+      label: 'Google',
+      value: 'google',
+      icon: () => (
+        <Image
+          source={require('../../../src/assets/icons/icon-google.png')}
+          style={{height: 20, width: 20}}
+        />
+      ),
+    },
+  ]);
+
+  const staticData: ICheckboxButton[] = [
+    {
+      id: 0,
+      fillColor: '#ff7473',
+      unfillColor: '#fbbfbb',
+      iconStyle: _iconStyle('#fbbfbb'),
+      iconImageStyle: styles.iconImageStyle,
+    },
+    {
+      id: 1,
+      fillColor: '#5567e9',
+      unfillColor: '#afb5f5',
+      iconStyle: _iconStyle('#afb5f5'),
+      iconImageStyle: styles.iconImageStyle,
+    },
+    {
+      id: 2,
+      fillColor: '#a98ae7',
+      unfillColor: '#cab6f4',
+      iconStyle: _iconStyle('#cab6f4'),
+      iconImageStyle: styles.iconImageStyle,
+    },
+    {
+      id: 3,
+      fillColor: '#fcb779',
+      unfillColor: '#ffd1a7',
+      iconStyle: _iconStyle('#ffd1a7'),
+      iconImageStyle: styles.iconImageStyle,
+    },
+    {
+      id: 4,
+      fillColor: '#2be055',
+      unfillColor: '#cbf2d5',
+      iconStyle: _iconStyle('#cbf2d5'),
+      iconImageStyle: styles.iconImageStyle,
+    },
+  ];
 
   return (
     <View style={styles.Container}>
       <Text style={styles.Heading}> Learn Digitally</Text>
+
+      <View style={styles.dropDownStyle}>
+        <Text>Social Media</Text>
+        <DropDownPicker
+          placeholder={'---Select---'}
+          // multiple={true}
+          // min={0}
+          // max={10}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          searchable={true}
+          theme="DARK"
+          dropDownContainerStyle={{
+            borderColor: 'white',
+          }}
+          searchContainerStyle={{
+            borderBottomWidth: 0,
+          }}
+          onSelectItem={item => {
+            console.log(item.value);
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          marginVertical: 20,
+        }}>
+        <BouncyCheckboxGroup
+          data={staticData}
+          onChange={(selectedItem: ICheckboxButton) => {
+            console.log('SelectedItem: ', JSON.stringify(selectedItem));
+          }}
+        />
+      </View>
+
       <View style={styles.InnerContainer}>
         <View style={styles.Item}>
           <Pressable onPress={() => navigation.navigate('Chapter')}>
@@ -76,6 +190,11 @@ const styles = StyleSheet.create({
     right: 10,
     top: 10,
     backgroundColor: '#D2DDDE',
+  },
+  dropDownStyle: {
+    // borderWidth: 1,
+    marginHorizontal: 20,
+    padding: 5,
   },
 });
 export default Home;
--- src/screens/Note/index.tsx
+++ src/screens/Note/index.tsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import Pdf from 'react-native-pdf';
-// import PDFView from 'react-native-view-pdf';
 
 const Note = ({route}: {route: any}) => {
   const {url} = route?.params;
-  const resourceType = 'url';
 
-  // console.log(url);
   return (
     <View>
       <Text>{url}</Text>
-      {/* <PDFView
-        fadeInDuration={250.0}
-        style={{flex: 1}}
-        resource={url}
-        resourceType={resourceType}
-        onLoad={() => console.log(`PDF rendered from ${resourceType}`)}
-        onError={error => console.log('Cannot render PDF', error)}
-      /> */}
       <Pdf
-        source={{
-          // uri: 'file:///Users/optimamacmini/sites/notesApp/src/assets/pdf/sample.pdf',
-          uri: 'https://api.printnode.com/static/test/pdf/multipage.pdf',
-          cache: false,
-        }}
-        style={styles.pdf}
         trustAllCerts={false}
+        source={{uri: url}}
         onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`number of pages: ${numberOfPages}`);
+          console.log(`Number of pages: ${numberOfPages}`);
         }}
         onPageChanged={(page, numberOfPages) => {
-          console.log(`current page: ${page}`);
+          console.log(`Current page: ${page}`);
         }}
         onError={error => {
           console.log(error);
         }}
+        onPressLink={uri => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={styles.pdf}
       />
     </View>
   );
@@ -43,10 +31,13 @@ const Note = ({route}: {route: any}) => {
 
 const styles = StyleSheet.create({
   pdf: {
-    flex: 1,
+    width: 300,
+    height: 300,
     borderWidth: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    margin: 10,
+    // width: Dimensions.get('window').width,
+    // height: Dimensions.get('window').height,
+    flex: 1,
   },
 });
 
